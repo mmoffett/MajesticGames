5b97b2348577b2a0ee69a3756d847aff1b0ed7ba
mmoffett <marissamoffett@gmail.com>
2016-03-16 21:13:40 -0700

    Created a Table In our SQLite DB
    Created High Score Table

--- src/SQLiteJDBC.java
+++ src/SQLiteJDBC.java
@@ -5,6 +5,7 @@ public class SQLiteJDBC
   public static void main( String args[] )
   {
     Connection c = null;
+    Statement stmt = null;
     try {
       Class.forName("org.sqlite.JDBC");
       c = DriverManager.getConnection("jdbc:sqlite:gameDB.db");
@@ -12,6 +13,22 @@ public class SQLiteJDBC
       System.err.println( e.getClass().getName() + ": " + e.getMessage() );
       System.exit(0);
     }
-    System.out.println("Opened database successfully");
+    
+    try {
+      stmt = c.createStatement();
+      String sql = "CREATE TABLE HighScores " +
+                   "(ID INT PRIMARY KEY NOT NULL," +
+                   " ScorerName VARCHAR(50), " + 
+                   " Score INT     , " + 
+                   " LevelTo INT, " + 
+                   " GameLength time(7))"; 
+      stmt.executeUpdate(sql);
+      stmt.close();
+      c.close();
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Table created successfully");
   }
 }
