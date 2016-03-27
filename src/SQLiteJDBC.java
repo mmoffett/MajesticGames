2f481ac43ec1ff9876a129aff8adcb11553a221f
mmoffett <marissamoffett@gmail.com>
2016-03-27 12:11:43 -0700

    moved from main to output function
    moved the code into a function that can be called in another class

--- src/SQLiteJDBC.java
+++ src/SQLiteJDBC.java
@@ -1,18 +1,13 @@
+import java.awt.Graphics;
 import java.sql.*;
+
 
 public class SQLiteJDBC
 {
-  public static void main( String args[] )
-  {
-    Connection c = null;
-    Statement stmt = null;
-    try {
-      Class.forName("org.sqlite.JDBC");
-      c = DriverManager.getConnection("jdbc:sqlite:gameDB.db");
-    } catch ( Exception e ) {
-      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-      System.exit(0);
-    }
+ // public static void main( String args[] )
+ // {
+    
+    
     
    /* try {
       stmt = c.createStatement();
@@ -58,33 +53,45 @@ public class SQLiteJDBC
         System.exit(0);
       }
       System.out.println("Records created successfully");*/
-    try {
-        c.setAutoCommit(false);
-
-        stmt = c.createStatement();
-        ResultSet rs = stmt.executeQuery( "SELECT * FROM HighScores;" );
-        while ( rs.next() ) {
-           int id = rs.getInt("id");
-           String  ScorerName = rs.getString("ScorerName");
-           int level  = rs.getInt("levelTo");
-           int  score = rs.getInt("score");
-           double gameLength = rs.getDouble("GameLength");
-           System.out.println( "ID = " + id );
-           System.out.println( "NAME = " + ScorerName );
-           System.out.println( "Level Reached = " + level );
-           System.out.println( "SCORE = " + score );
-           System.out.println( "Game Length = " + gameLength );
-           System.out.println();
-        }
-        rs.close();
-        stmt.close();
-        c.close();
-      } catch ( Exception e ) {
-        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-        System.exit(0);
-      }
-      System.out.println("Operation done successfully");
-    }
     
+      //System.out.println("Operation done successfully");
+   // }
+    public void output(Graphics g)
+    {
+    	Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:gameDB.db");
+          } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+          }
+    	try {
+            c.setAutoCommit(false);
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM HighScores;" );
+            while ( rs.next() ) {
+               int id = rs.getInt("id");
+               String  ScorerName = rs.getString("ScorerName");
+               int level  = rs.getInt("levelTo");
+               int  score = rs.getInt("score");
+               double gameLength = rs.getDouble("GameLength");
+               System.out.println( "ID = " + id );
+               System.out.println( "NAME = " + ScorerName );
+               System.out.println( "Level Reached = " + level );
+               System.out.println( "SCORE = " + score );
+               System.out.println( "Game Length = " + gameLength );
+               System.out.println();
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+          } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+          }
+    }
     
   }
