35aa3731d644cc7150788053f056c6e99155faf1
mmoffett <marissamoffett@gmail.com>
2016-04-03 22:10:11 -0700

    updated keyPressed in MyKeyListener

--- src/MyKeyListener.java
+++ src/MyKeyListener.java
@@ -4,11 +4,26 @@ import java.awt.event.KeyListener;
 
 class MyKeyListener implements KeyListener
 {
+	Animation a;
+	
+	public MyKeyListener()
+	{
+		Animation a=new Animation();
+	}
+	
 	@Override
 	public void keyPressed(KeyEvent event)
 	{
-		// TODO
-		
+		 if(event.getKeyCode() == KeyEvent.VK_RIGHT){
+		        System.out.println("Pressed RIGHT");
+		        a.moveRight();
+		    }
+		    if(event.getKeyCode() == KeyEvent.VK_LEFT){
+		        System.out.println("Pressed LEFT");
+		        a.moveLeft(); 
+		    }
+		//String key=KeyStroke.getKeyStrokeForEvent(event).toString();
+		//key=key.replace("pressed","");
 	}
 
 	@Override
