51d0cf1f9fd0dfaa8b5d24d40f1802e0de39f91f
mmoffett <marissamoffett@gmail.com>
2016-04-03 21:40:00 -0700

    created a class to implement KeyListener

---
+++ src/MyKeyListener.java
@@ -0,0 +1,27 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+class MyKeyListener implements KeyListener
+{
+	@Override
+	public void keyPressed(KeyEvent event)
+	{
+		// TODO
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event)
+	{
+		// TODO 
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent event) 
+	{
+		// TODO 
+		
+	}
+}
