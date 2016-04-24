acf28224a38bfdf170701520aa835f22ed2ac8f9
mmoffett <marissamoffett@gmail.com>
2016-04-24 14:31:46 -0700

    removes unused imports

--- src/AnimationPane.java
+++ src/AnimationPane.java
@@ -7,13 +7,9 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
-import java.io.File;
-import java.io.IOException;
 import java.util.Vector;
 
-import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
 import javax.swing.ActionMap;
 import javax.swing.ImageIcon;
